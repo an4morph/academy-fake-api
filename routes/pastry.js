2626860d500b171e545b920c23f47c27388a2af8
antonova.polina <antonova.polina@clover.global>
2021-02-01 13:27:30 +0600

    add error checks in pastry.create

--- routes/pastry.js
+++ routes/pastry.js
@@ -7,16 +7,34 @@ const getAll = (req, res) => {
   res.send(pastry)
 }
 
+function isObject (value) {
+  return value && typeof value === 'object' && value.constructor === Object;
+}
+
 const createNew = (req, res) => {
-  const { name, inStock } = req.body
+  const { name, inStock, ingredients, image, address, hasDelivery } = req.body
   if (!name) return error(res, 400, 'name attribute is required')
   if (!inStock) return error(res, 400, 'inStock attribute is required')
+
+  if (typeof name !== 'string') return error(res, 400, 'name attribute should be type `string`')
+  if (typeof inStock !== 'number') return error(res, 400, 'inStock attribute should be type `number`')
+  if (ingredients && !Array.isArray(ingredients)) return error(res, 400, 'ingredients attribute should be type `array`')
+  if (image && typeof image !== 'string') return error(res, 400, 'image attribute should be type `string`')
+  if (hasDelivery && typeof hasDelivery !== 'boolean') return error(res, 400, 'hasDelivery attribute should be type `boolean`')
+
+  const invalidAddressAttr = Object.keys(req.body.address).find(attr => !['street', 'houseNumber'].includes(attr))
+  
+  if (address) {
+    if (!isObject(address)) return error(res, 400, 'address attribute should be type `object`')
+    if (invalidAddressAttr) return error(res, 400, `'${invalidAddressAttr}' is invalid attribute in address object`)
+    if (address.houseNumber && typeof address.houseNumber !== 'number') return error(res, 400, 'address.houseNumber attribute should be type `number`')
+    if (address.street && typeof address.street !== 'string') return error(res, 400, 'address.street attribute should be type `string`')
+  }
 
   const newItem = { 
     id: shortid.generate(),
     name,
     inStock,
-    isFavorite: isFavorite || false,
     ingredients: ingredients || [],
     image: image || null,
     address: address || null,
@@ -29,5 +47,5 @@ const createNew = (req, res) => {
 
 module.exports = {
   getAll,
-  createNew
+  createNew,
 }
