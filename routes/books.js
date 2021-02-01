039176784439ed521b709ce51af2e718bf673f58
antonova.polina <antonova.polina@clover.global>
2021-02-01 23:11:40 +0600

    add type check to books api

--- routes/books.js
+++ routes/books.js
@@ -19,6 +19,15 @@ const createNew = (req, res) => {
   if (!name) return error(res, 400, '`name` attribute is required')
   if (!author) return error(res, 400, '`author` attribute is required')
 
+  if (typeof name !== 'string') return error(res, 400, 'name attribute should be type `string`')
+  if (typeof author !== 'string') return error(res, 400, 'author attribute should be type `string`')
+  if (isFavorite && typeof isFavorite !== 'boolean') return error(res, 400, 'isFavorite attribute should be type `boolean`')
+  if (publishYear && typeof publishYear !== 'number') return error(res, 400, 'publishYear attribute should be type `number`')
+  if (publishHouse && typeof publishHouse !== 'boolean') return error(res, 400, 'publishHouse attribute should be type `string`')
+  if (pagesNumber && typeof pagesNumber !== 'number') return error(res, 400, 'pagesNumber attribute should be type `number`')
+  if (genres && !Array.isArray(genres)) return error(res, 400, 'genres attribute should be type `array`')
+  if (originalLanguage && typeof originalLanguage !== 'string') return error(res, 400, 'originalLanguage attribute should be type `string`')
+
   const newBook = { 
     name,
     author,
@@ -38,6 +47,7 @@ const createNew = (req, res) => {
 
 const updateItem = (req, res) => {
   const { id } = req.params
+  const { name, author, isFavorite, publishYear, publishHouse, pagesNumber, genres, originalLanguage } = req.body
   const permisbleKeys = 'name, author, isFavorite, publishYear, publishHouse, pagesNumber, genres, originalLanguage'.split(', ')
 
   const book = db.get('books').find({ id }).value()
@@ -46,10 +56,18 @@ const updateItem = (req, res) => {
 
   const keys = Object.keys(req.body)
   const invalid = keys.filter(k => !permisbleKeys.includes(k))
-
   if (invalid.length) {
     return error(res, 400, `${invalid.join(', ')} is not valid keys`)
   }
+
+  if (name && typeof name !== 'string') return error(res, 400, 'name attribute should be type `string`')
+  if (author && typeof author !== 'string') return error(res, 400, 'author attribute should be type `string`')
+  if (isFavorite && typeof isFavorite !== 'boolean') return error(res, 400, 'isFavorite attribute should be type `boolean`')
+  if (publishYear && typeof publishYear !== 'number') return error(res, 400, 'publishYear attribute should be type `number`')
+  if (publishHouse && typeof publishHouse !== 'boolean') return error(res, 400, 'publishHouse attribute should be type `string`')
+  if (pagesNumber && typeof pagesNumber !== 'number') return error(res, 400, 'pagesNumber attribute should be type `number`')
+  if (genres && !Array.isArray(genres)) return error(res, 400, 'genres attribute should be type `array`')
+  if (originalLanguage && typeof originalLanguage !== 'string') return error(res, 400, 'originalLanguage attribute should be type `string`')
 
   const updatedBook = { ...book, ...req.body }
 
